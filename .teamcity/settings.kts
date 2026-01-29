fbf6504ba8437d3eebbdb029f307665fa1954c93
Nastasia Chubatova <78346593+ChubatovaTiger@users.noreply.github.com>
2026-01-29 15:59:36 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -3,29 +3,22 @@ import jetbrains.buildServer.configs.kotlin.*
 version = "2025.11"
 
 project {
-
     buildType(Source)
-
-    val numProjects=1
     val numConfigurationsPerProject=900
-
-
     for (i in 0..numProjects) {
         subProject {
             id("subProj_$i")
             name = "subProj $i"
 
-            for (j in 0..numConfigurationsPerProject) {
+         for (j in 0..numConfigurationsPerProject) {
                 buildType {
                     id("subProj_bt_$i" + "_$j")
-                    name = "bt $i $j"
-    }
+                    name = "bt $i $j"}
 
-                }
-            }
+         }
         }
-    }
-
+     }
+}
 
 object Source : BuildType({
     name = "source"
@@ -34,12 +27,9 @@ object Source : BuildType({
                 dependencies {
                     snapshot(AbsoluteId("subProj_bt_$i" + "_$j")) {
                     reuseBuilds = ReuseBuilds.NO
+                    }
                 }
-
-                }
-}
-
-
+            }
 })
 
 
